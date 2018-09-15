bd4f49472cb504f94d2c2bb3c3a1aca74af2e156
Daryl Hamilton <daryl.hamilton@outlook.com>
2018-09-16 00:28:08 +1200

    Fixing update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,17 +8,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  db.collection('Todos').findOneAndUpdate({
-    _id: new ObjectID('5b9c91ed755f4f84c0c97eff')
-  }, {
-    $set: {
-      completed: true
-    }
-  }, {
-    returnOriginal: false
-  }).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b9c91ed755f4f84c0c97eff')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
   db.collection('Users').findOneAndUpdate({
     _id: new ObjectID('5b98c3d234b22b071a4e9511')
