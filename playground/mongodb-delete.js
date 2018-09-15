3da7fc641a87449d02ba4db76932fc6338cc71bf
Daryl Hamilton <daryl@Daryls-iMac-1246.local>
2018-09-16 00:00:48 +1200

    Add deltet script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) =>{
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+//   db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//     console.log(result);
+//   });
+  db.collection('Users').deleteMany({name: 'Daryl'}).then((result) =>{
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5b9a0f4f84a4ae0f0b714165')
+  }).then((result) => {
+    console.log(result);
+  });
+  // client.close();
+});
