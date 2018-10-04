271f1e677818ee3b564dffa24dd5b700470e6cd4
Daryl Hamilton <daryl.hamilton@outlook.com>
2018-10-04 18:53:27 +1300

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -53,42 +55,42 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-  app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id;
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-    if(!ObjectID.isValid(id)) {
-      return res.status(404).send();
-    }
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-      Todo.findByIdAndRemove(id).then((todo) => {
-        if (!todo) {
-          return res.status(404).send();
-        }
-        res.send({todo});
-    }).catch((e) => {
-      res.status(400).send();
-    });
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
+});
 
 app.patch('/todos/:id', (req, res) => {
-  var id = request.params.id;
+  var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  if (_.ifBoolean(body.completed) && body.completed) {
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
-  }
+  };
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
-    }
+    };
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
